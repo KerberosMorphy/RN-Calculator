4bcd7af1e9459e2419e957e4a9210fc946987a6c
Benoit Verret <morphy001@hotmail.com>
2018-10-13 14:35:24 -0400

    C, CE, float implementation, number after = issues corrected

--- ReactCalculator/ReactCalculator.js
+++ ReactCalculator/ReactCalculator.js
@@ -1,16 +1,147 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { 
+    View,
+    Text
+} from 'react-native';
 import Style from './Style';
+import InputButton from './InputButton';
 
+const inputButtons = [
+    [1, 2, 3, 'CE', 'C'],
+    [4, 5, 6, '/', '+'],
+    [7, 8, 9, '*', '-'],
+    ['(', ')', 0,  '.', '=']
+];
+
+/**
+ * For more details see {@link https://kylewbanks.com/blog/react-native-tutorial-part-1-hello-react ReactCalculator} turtorial.
+ * You can find the complete code on my {@link https://github.com/KerberosMorphy/RN-Calculator GitHub}.
+ */
 export default class ReactCalculator extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.initialState = {
+            previousInputValue: 0,
+            inputValue: 0,
+            selectedSymbol: null,
+            resultState: false,
+            decimalState: 0
+        }
+
+        this.state = this.initialState;
+    }
 
     render() {
         return (
             <View style={Style.rootContainer}>
-                <View style={Style.displayContainer}></View>
-                <View style={Style.inputContainer}></View>
+                <View style={Style.displayContainer}>
+                    <Text style={Style.displayText}>{this.state.inputValue}</Text>
+                </View>
+                <View style={Style.inputContainer}>
+                    {this._renderInputButtons()}
+                </View>
             </View>
         )
     }
 
+    _renderInputButtons() {
+
+        let views = inputButtons.map((row, idx) => {
+            let inputRow = row.map((buttonVal, columnIdx) => {
+                return<InputButton
+                        value={buttonVal}
+                        highlight={this.state.selectedSymbol === buttonVal}
+                        onPress={this._onInputButtonPressed.bind(this, buttonVal)}
+                        key={'butt-' + columnIdx} />;
+            });
+
+            return <View style={Style.inputRow} key={'row-' + idx}>{inputRow}</View>
+        });
+
+        return views;
+    }
+
+    _onInputButtonPressed(input) {
+        switch (typeof input) {
+            case 'number':
+                return this._handleNumberInput(input)
+            default:
+                return this._handleStringInput(input)
+        }
+    }
+
+    _handleNumberInput(num) {
+        let inputValue = num;
+        if (!this.state.resultState) {
+            if (this.state.decimalState === 0) {
+                inputValue = (this.state.inputValue * 10) + num;
+            }
+            else {
+                inputValue = (this.state.inputValue) + (num / (10 * this.state.decimalState));
+                this.state.decimalState ++;
+            }
+        }
+        this.setState({
+            inputValue: inputValue,
+            resultState: false
+        });
+    }
+
+    _handleStringInput(str) {
+        switch (str) {
+            case '/':
+            case '*':
+            case '+':
+            case '-':
+                this.setState({
+                    selectedSymbol: str,
+                    previousInputValue: this.state.inputValue,
+                    inputValue: 0,
+                    decimalState: 0,
+                    resultState: false
+                });
+                break;
+            
+            case '.':
+                this.setState({
+                    decimalState: 1,
+                    resultState: false
+                });
+                break;
+
+            case '=':
+                let symbol = this.state.selectedSymbol,
+                    inputValue = this.state.inputValue,
+                    previousInputValue = this.state.previousInputValue;
+
+                if (!symbol) {
+                    return;
+                }
+
+                this.setState({
+                    previousInputValue: 0,
+                    inputValue: eval(previousInputValue + symbol + inputValue),
+                    selectedSymbol: null,
+                    decimalState: 0,
+                    resultState: true
+                });
+                break;
+
+            case 'CE':
+                this.setState(this.initialState);
+                break;
+
+            case 'C':
+                if (this.state.resultState) {
+                    this.setState(this.initialState);
+                }
+                this.setState({
+                    inputValue: 0,
+                    decimalState: 0
+                });
+                break;
+        }
+    }
 }
